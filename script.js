e2d1af1caba2d4a2faeed0edc60a166c7b033f55
Alepss <168598025+Alepss@users.noreply.github.com>
2025-03-25 15:36:31 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Función para la cuenta regresiva
+const countdownDate = new Date("March 29, 2025 16:00:00").getTime();
+
+const countdownTimer = setInterval(function() {
+    const now = new Date().getTime();
+    const distance = countdownDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown-timer").innerHTML = 
+        days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownTimer);
+        document.getElementById("countdown-timer").innerHTML = "¡Es hora de la revelación!";
+    }
+}, 1000);
+
+import { database } from "./firebase-config.js"; 
+
+console.log("Firebase cargado correctamente", database);
